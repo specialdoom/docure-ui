87ae7e2ffaad3fd4bbbe325de1b2a90d72b6d61d
Bogdan Bledea <bledea.bogdan97@gmail.com>
2021-05-28 17:50:43 +0300

    Add all components. Prepare final script for bundle

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '@specialdoom/docure-button';
+import '@specialdoom/docure-article-card';
+import '@specialdoom/docure-banner';
+import '@specialdoom/docure-header';
+import '@specialdoom/docure-list';
+import '@specialdoom/docure-loading';
+import '@specialdoom/docure-result';
